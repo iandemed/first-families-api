b876981a37e2b643e1a9124450a2dc5fa6bed632
iandemed <iandemed@gmail.com>
2020-09-09 16:17:47 -0400

    Added the base route to list all of the presidents

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 let router = new express.Router()
 
+const presidentController = require('../controllers/president')
+
+
 router.use('/president', require('./president'))
+router.get('/', presidentController.index)
 
 module.exports = router
