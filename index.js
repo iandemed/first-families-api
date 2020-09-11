d0059db4e3dd35f5854a0e7b656a80da7839946b
iandemed <iandemed@gmail.com>
2020-09-11 14:48:09 -0400

    Added function to allow for access to API from localhost during testing

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const express = require('express')
 let app = express()
 
 app.use(express.json())
+
+// Allows CORS requests for the GET, POST, PUT, and DELETE methods
+let cors = function(request, response, next) {
+    response.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+    response.header("Access-Control-Allow-Origin", "*")
+    if(request.method == 'OPTIONS') response.end()
+    else next()
+  }
+
+app.use(cors) 
+  
 app.use("/", require("./routes/index"))
 
 app.set("port", process.env.PORT || 8080)
