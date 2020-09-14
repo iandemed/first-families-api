c82cd4053ff64cf1cd2da7a3415678cf38886855
iandemed <iandemed@gmail.com>
2020-09-14 19:09:18 -0400

    Remove self-written function to npm package

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 
 let app = express()
 
 app.use(express.json())
 
-// Allows CORS requests for the GET, POST, PUT, and DELETE methods
-let cors = function(request, response, next) {
-    response.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-    response.header("Access-Control-Allow-Origin", "*")
-    if(request.method == 'OPTIONS') response.end()
-    else next()
-  }
-
-app.use(cors) 
+// Enable all CORS requests with npm package
+app.use(cors()) 
   
 app.use("/", require("./routes/index"))
 
